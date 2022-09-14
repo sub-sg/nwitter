30db97b434a6ecd86aa04c45bc70bfce1348792f
sub-sg <dev.soogom@gmail.com>
2022-09-15 00:05:22 +0900

    Using Firebase Authentication

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,12 @@
 // mysite/Auth
 import React from 'react';
 import { useState } from 'react';
+import { authService } from '../fbase';
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
 
     const onChange = (event) => {
         // console.log(event.target.name);
@@ -19,8 +21,22 @@ const Auth = () => {
         }
     };
 
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault(); // onSubmit 함수에서 이벤트의 기본값을 막음 (기본값인 페이지 새로고침 방지)
+        try {
+            let data;   
+            if (newAccount) {
+                // create new account
+                data = await authService.createUserWithEmailAndPassword(email, password);
+                //authService.setPersistence('session');    // local(default), session, none
+            } else {
+                // log in
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
@@ -29,7 +45,7 @@ const Auth = () => {
                 <form onSubmit={onSubmit}>
                     <input name='email' type='email' placeholder='Email' required value={email} onChange={onChange}/>
                     <input name='password' type='password' placeholder='Password' required  value={password} onChange={onChange}/>
-                    <input type='submit' value='Log In' />
+                    <input type='submit' value={newAccount ? 'Create Account' : 'Log In'} />
                 </form>
             </div>
             <div>
